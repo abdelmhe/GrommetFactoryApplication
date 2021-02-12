16c39062914c807c05067de9c10fa7044a73af69
abdelmhe <abdelmhe@sheridancollege.ca>
2021-02-12 18:45:06 +0200

    Second Commit

--- src/main/java/ca/sheridancollege/abdelmhe/controllers/GrommetController.java
+++ src/main/java/ca/sheridancollege/abdelmhe/controllers/GrommetController.java
@@ -1,7 +1,23 @@
 package ca.sheridancollege.abdelmhe.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GrommetController {
+    @GetMapping("/")
+    public String index(){
+
+
+        return "index";
+    }
+
+    @PostMapping ("/grommetFactory")
+
+    public String processGrommets(){
+
+        return "DisplayGrommets";
+    }
 }
